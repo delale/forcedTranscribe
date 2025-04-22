3545498de150f9cb8fdffdc9b3150fa19b1034db
delale@MacBookPro <alessandro.deluca@uzh.ch>
2025-04-22 17:01:02 +0200

    added device settings for GPU usage

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,8 +2,8 @@ import torch
 
 # Set device
 if torch.cuda.is_available():
-    device = "cuda:0"  # CUDA GPU
+    device = torch.device("cuda:0")  # CUDA GPU
 elif torch.mps.is_available():
-    device = "mps"  # Apple silicon GPU
+    device = torch.device("mps")  # Apple silicon GPU
 else:
-    device = "cpu"
+    device = torch.device("cpu")  # CPU
